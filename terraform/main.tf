46627dfdc12dfe2cef94979f9525299bc2788d13
Dmytro Ronevych <dmytro874@gmail.com>
2026-04-06 21:25:34 +0300

    fixed several states for grants

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,15 +14,16 @@ provider "databricks" {
   token = var.databricks_token
 }
 
-resource "databricks_grant" "admin_access" {
-  # Вказуємо повний шлях до існуючої схеми текстом
-  schema     = "dbr_dev.ronevych_test"
-  principal  = "lbiel@softserve.academy" 
-  privileges = ["ALL_PRIVILEGES"]
-}
+resource "databricks_grant" "schema_permissions" {
+  schema = "dbr_dev.ronevych_test"
 
-resource "databricks_grant" "others_browse" {
-  schema     = "dbr_dev.ronevych_test"
-  principal  = "account users"
-  privileges = ["USAGE"] 
+  grant {
+    principal  = "lbiel@softserve.academy"
+    privileges = ["ALL_PRIVILEGES"]
+  }
+
+  grant {
+    principal  = "account users"
+    privileges = ["USE_SCHEMA", "SELECT"]
+  }
 }
